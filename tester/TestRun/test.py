685a0d252cb74a5df6065517cae3b298d5df7643
Sumit Kumar Dutta <sumit.dutta@olacabs.com>
2017-02-05 23:33:16 +0530

    changing to shoppersstop

--- tester/TestRun/test.py
+++ tester/TestRun/test.py
@@ -138,7 +138,7 @@ def testFeed(userprefs):
     colors = userprefs['colors']
     types = userprefs['types']
     skinTone = userprefs['skinTone']
-    domains = ["Jabong"]
+    domains = ["ShoppersStop"]
 
     if userprefs["gender"] == "Female":
 
