17afeeb4ae9bea4684d445169549071797024211
Sumit Kumar Dutta <sumit.dutta@olacabs.com>
2017-11-15 00:02:54 +0530

    Rating messages

--- Services/RatingService.py
+++ Services/RatingService.py
@@ -4,34 +4,40 @@ import IntelligenceLayer.RuleEngine.ruleEngine as re
 import DatabaseLayer.converseWithDB as cdb
 import random
 
-high = ["Wow! This will look great on you.",
-    "The perfect product to complement your style.",
-    "Put your most stylish foot forward with this product.",
-    "You have a good eye for style.",
-    "Nice! You know how to pick em'.",
-    "Looks like you have your personal style all figured out.",
-    "You're going to make all the right moves in this product."
-    "We love this product for you!",
-    "Oh! You're going to look smooth in this." ]
+# high = ["Wow! This will look great on you.",
+#     "The perfect product to complement your style.",
+#     "Put your most stylish foot forward with this product.",
+#     "You have a good eye for style.",
+#     "Nice! You know how to pick em'.",
+#     "Looks like you have your personal style all figured out.",
+#     "You're going to make all the right moves in this product."
+#     "We love this product for you!",
+#     "Oh! You're going to look smooth in this." ]
+#
+# medium = ["Good choice, you sure have style.",
+#     "The perfect product to complement your style.",
+#     "Put your most stylish foot forward with this product.",
+#     "A good piece to add to your wardrobe.",
+#     "This choice has great fashion potential",
+#     "You can mix and match this with ease.",
+#     "You're going to look great in this.",
+#     "You have the perfect product now to find an occasion."]
+#
+# low = ["Find something that will match your style.",
+#     "Try a different product, maybe?",
+#     "A different product might be better suited for you.",
+#     "Try something in a different colour.",
+#     "Perfect clothes are hard to find sometimes.",
+#     "Hmm.. looks like this isn't the one.",
+#     "This might not be the best choice for you.",
+#     "Oh no! Try another product, it might be a better match to your style."]
+#
 
-medium = ["Good choice, you sure have style.",
-    "The perfect product to complement your style.",
-    "Put your most stylish foot forward with this product.",
-    "A good piece to add to your wardrobe.",
-    "This choice has great fashion potential",
-    "You can mix and match this with ease.",
-    "You're going to look great in this.",
-    "You have the perfect product now to find an occasion."]
+high = ["Wow! This will look great on you",
+        "The perfect product to complement your style",
+        "Good choice! You sure know how to pick em'"]
 
-low = ["Find something that will match your style.",
-    "Try a different product, maybe?",
-    "A different product might be better suited for you.",
-    "Try something in a different colour.",
-    "Perfect clothes are hard to find sometimes.",
-    "Hmm.. looks like this isn't the one.",
-    "This might not be the best choice for you.",
-    "Oh no! Try another product, it might be a better match to your style."   ]
-
+low = ["Find a product that matches your style"]
 
 def getMessage(list):
     index = random.randint(0,len(list) - 1)
@@ -41,13 +47,13 @@ def getMessage(list):
 
 def getRatingMessage(score):
     message = ""
-    if score >= .7:
+    if score >= .4:
         message = getMessage(high)
 
-    if score >= .5 and score < .7:
-        message = getMessage(medium)
+    # if score >= .5 and score < .7:
+    #     message = getMessage(medium)
 
-    if score < .5:
+    if score < .4:
         message = getMessage(low)
 
     return message
